e7334a2da0376dd2e11ed94daa920c5b323bc04b
LucaOverflow <luca@pschl.net>
2026-07-18 02:51:43 +0200

    Extract params to their own file for now These should be provided via command line or config file in the end

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import * as zmq from "zeromq"
 import { queueMessage } from "./router.js"
+import { decoderAddress } from "./params.js"
 
 async function main () {
     const socket = new zmq.Subscriber()
-    const decoderAddress = "tcp://rc-timing-start:5556"
     console.log("Listening to Decoder from", decoderAddress)
     socket.connect(decoderAddress)
     socket.subscribe()
